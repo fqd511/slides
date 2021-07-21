0d0579d2168becf16fa2e797813a99a431970287
fanqidi <fqd@xkool.org>
2021-07-21 18:59:26 +0800

    feat(base): update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+# 流程：
+# 1.获取分支名称并打包
+# 2.将部署文件拷贝到__temp__下对应目录
+# 3.切换分支到 homepage
+# 4.执行 sh update $branch
+
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -9,24 +16,26 @@ branch=$(git symbolic-ref --short HEAD)
 echo "Current branch: $branch"
 
 # 生成部署文件,注意路径单独注明
-#slidev build --base /slides/$branch/
-slidev build --base /slides/
+slidev build --base /slides/$branch/
+#slidev build --base /slides/
 
-# 进入生成的文件夹
-cd dist
+# refresh branch folder
+rm -rf __temp__/$branch
+mkdir  __temp__/$branch
+
 
 # 如果是发布到自定义域名
 # echo 'www.digvan.com' > CNAME
 
-git init
-git add -A
-git commit -m "deploy branch: $branch"
+# git init
+# git add -A
+# git commit -m "deploy branch: $branch"
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f https://github.com/<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/fqd511/slides.git master:gh-pages
+# git push -f https://github.com/fqd511/slides.git master:gh-pages
 
-#remove .git folder(and all its content) in dist
-#rm -rf .git
+# remove .git folder(and all its content) in dist
+# rm -rf .git
