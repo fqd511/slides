e3455bc2bf465bc6a8fd83f4192c0e33235b840a
511 <fqd511@163.com>
2021-08-06 12:00:47 +0800

    feat(homepage): comment in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 流程：
-# 1. node update link_example 更新 db.json
+# 1. node update link desc 更新 db.json
 # 2. npm run build 输出静态部署文件到 dist
 # 3. 将 link_example对应的部署文件移到 sub 下（覆盖:先删掉原来的，再移入）[提交 homepage 分支]
 # 4. 复制 sub 下目录到 dist
