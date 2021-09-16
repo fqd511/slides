48875cb09cbffd0c61a60399d6798652292f45e3
511 <fqd511@gmail.com>
2021-09-16 23:19:00 +0800

    feat(homepage): update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,8 @@
 
 # 流程：
 # 1. node update link(slide 代号) desc(slide 介绍) 更新 db.json，并提交 homepage 分支
-# 2. npm run build 输出静态部署文件到 dist
+# 2. npm run install && npm run build 输出静态部署文件到 dist
 # 3. 将 link 对应的部署文件(在__temp__/link下)移到 dist 的 sub 下（覆盖:先删掉原来的，再移入）
 # 4. 进入 dist 部署到 gh-pages
 
-echo "$1"
-echo "$2"
-echo "$3"
+echo "update slide ($1) with description ($2)"
